99ccdf7a1be9d2924e8f61c59e8712b5e28ba009
yaxian.gu <yaxian.gu@gmail.com>
2022-04-05 17:05:17 +0800

    feat: diesel_migrations

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,43 @@ extern crate diesel;
 use diesel::pg::PgConnection;
 
 #[macro_use]
+extern crate diesel_migrations;
+
+#[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
+use rocket::fairing::AdHoc;
 use rocket_contrib::database;
+
+#[macro_use]
+extern crate log;
 
 mod api;
 mod constants;
+mod macros;
 mod models;
 mod schema;
 mod services;
-mod macros;
 
 use api::post_controller::routes as post_routes;
 
+embed_migrations!();
 #[database("diesel_posts")]
 pub struct PostsDbConn(PgConnection);
 
 fn main() {
     rocket::ignite()
         .attach(PostsDbConn::fairing())
+        .attach(AdHoc::on_attach("Database Migrations", |rocket| {
+            let conn = PostsDbConn::get_one(&rocket).expect("Expect postgres database connection");
+            match embedded_migrations::run(&*conn) {
+                Ok(()) => Ok(rocket),
+                Err(e) => {
+                    error!("Failed to run database migrations: {:?}", e);
+                    Err(rocket)
+                }
+            }
+        }))
         .mount("/posts", post_routes())
         .launch();
 }
